59c1561177d5a345587c59ee07b13cf86f5b5a88
Kalpathi Subramanian <kalpathi@cci0vjg1j2aws.uncc.edu>
2019-02-15 09:06:07 -0500

    osm classes

---
+++ src/data_src/OSMEdge.h
@@ -0,0 +1,99 @@
+#ifndef OSM_EDGE_H
+
+#define OSM_EDGE_H
+
+#include <string>
+
+using namespace std;
+
+namespace bridges {
+	/**
+	 * @brief  Class that hold Open Street Map edges
+	 *
+	 * Class that holds Open Street Map edges from https://openstreetmap.org
+	 *
+	 * Kalpathi Subramanian, 2/14/19
+	 *
+	 */
+
+
+	class OSMEdge {
+
+		private:
+							// edge vertices
+			int src_vertex,	dest_vertex;	
+							// edge length
+			double length;
+
+		public:
+
+			OSMEdge() : src_vertex(0), dest_vertex(0), length(0.0) {
+			}
+
+			OSMEdge(int src, int dest, double dist) : 
+				src_vertex(src), dest_vertex(dest), length(dist) {
+			}
+
+			OSMEdge(const OSMEdge *edge) : src_vertex(edge->src_vertex), 
+				dest_vertex(edge->dest_vertex), length(edge->length) {
+			}
+
+			/**
+			 *	get source vertex  of edge
+			 *
+			 *	@return int
+			 *
+			 */
+			int getSourceVertex() const {
+				return src_vertex;
+			}
+			/**
+			 *	set source vertex  of edge
+			 *
+			 *	@param int
+			 *
+			 */
+			void setSourceVertex(int src) {
+				src_vertex = src;
+			}
+			/**
+			 *	get destination vertex of edge
+			 *
+			 *	@return int
+			 *
+			 */
+			int getDestinationVertex() const {
+				return dest_vertex;
+			}
+			/**
+			 *	set destination vertex of edge
+			 *
+			 *	@param int
+			 *
+			 */
+			void setDestinationVertex(int dest) {
+				this->dest_vertex = dest;
+			}
+			/**
+			 *	get edge length of edge
+			 *
+			 *	@return double
+			 *
+			 */
+			double getEdgeLength() const {
+				return length;
+			}
+			/**
+			 *	set edge length
+			 *
+			 *	@param double
+			 *
+			 */
+			void setDestinationVertex(double l) {
+				this->length = l;
+			}
+	};
+
+} // namespace bridges
+
+#endif
---
+++ src/data_src/OSMVertex.h
@@ -0,0 +1,94 @@
+#ifndef OSM_VERTEX_H
+
+#define OSM_VERTEX_H
+
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+namespace bridges {
+	/**
+	 * @brief  Class that hold Open Street Map vertices
+	 *
+	 * Class that holds Open Street Map vertices from https://openstreetmap.org
+	 *
+	 * Kalpathi Subramanian, 2/14/19
+	 *
+	 */
+
+
+	class OSMVertex {
+
+		private:
+			double latitude;		// latitude 
+			double longitude;		// longitude
+
+			double degreeToRadians(double deg) {
+				return deg*M_PI/180.;
+			}
+		public:
+
+			OSMVertex() : latitude(0.0), longitude(0.0) {
+			}
+
+			OSMVertex (double longit, double latit) 
+				: latitude(latit), longitude(longit) {
+			}
+
+			OSMVertex(const OSMVertex *vert)
+				: latitude(vert->latitude), longitude(vert->longitude){
+			}
+
+			/**
+			 *	get latitude  of node
+			 *
+			 *	@return double
+			 *
+			 */
+			double getLatitude() const {
+				return latitude;
+			}
+			/**
+			 *	set latitude of quake location
+			 *
+			 *	@param latit latitude of node
+			 *
+			 */
+			void setLatitude(double latit) {
+				latitude = latit;
+			}
+			/**
+			 *	get longitude of quake location
+			 *
+			 *	@return longitude of node
+			 *
+			 */
+			double getLongitude() const {
+				return longitude;
+			}
+			/**
+			 *	set longitude of quake location
+			 *
+			 *	@param (double)
+			 *
+			 */
+			void setLongitude(double longit) {
+				this->longitude = longit;
+			}
+			/** 
+			 * 	convert lat/long coords to Cartesian
+			 *
+			 */
+			void getCartesianCoords(double *cartesian_coords) {
+				double R = 6378.; // Radius of the earth in km
+				double lat_rad  = degreeToRadians(latitude);
+				double longit_rad  = degreeToRadians(longitude);
+				cartesian_coords[0] = R * cos(lat_rad) * cos (longit_rad);
+				cartesian_coords[1] = R * cos(lat_rad) * sin (longit_rad);
+			}
+	};
+
+} // namespace bridges
+
+#endif
