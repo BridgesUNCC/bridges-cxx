abea3bc7304d8ce942b5cc52c23ba36febecaf40
Erik Saule <esaule@uncc.edu>
2019-07-12 14:50:21 -0400

    documenting GameBase

--- src/GameBase.h
+++ src/GameBase.h
@@ -34,16 +34,35 @@ namespace bridges {
 	std::cerr<<"nbRow: "<<nbRow<<" nbCol: "<<nbColumn<<std::endl;
     }
 
-
+    /// @brief This function is called once when the game starts.
+    ///
+    /// Students write this function.
+    /// It will be called once at the begining of the game.
     virtual void initialize () = 0;
+
+    /// @brief This function is called once per frame of the game.
+    ///
+    /// Students write this function.
+    /// It will be called at each frame of the game.
     virtual void GameLoop () = 0;
 
     
   protected:
+    /// @brief register a new KeypressListener
+    ///
+    /// Students should not have to call this function directly.  The
+    /// KeypressListener listener will get notified of all keypresses
+    /// (up and down) that happens in the game.
+    ///
+    /// @param kl a KeypressListener to register
     void registerKeyListener(KeypressListener* p) {
       sockcon->registerKeyListener(p);
     }
 
+    /// @brief Renders the game
+    ///
+    /// Student should not have to call this function directly. It is
+    /// called automatically by Bridges.
     void render() {
       if (firsttime) {
 	bridges.setVisualizeJSONFlag(true);
@@ -60,10 +79,21 @@ namespace bridges {
     }
 
   protected:
+    /// @brief Change the background color of a cell
+    ///
+    /// @param x row of the cell to set
+    /// @param y column of the cell to set
+    /// @param c NamedColor to set
     void setBGColor(int row, int col, NamedColor nc) {
       gg.setBGColor(row, col, nc);
     }
-
+    
+    /// @brief Draw an object on the game
+    ///
+    /// @param x row of the cell to draw the object on
+    /// @param y column of the cell to draw the object on
+    /// @param s symbol representing the object
+    /// @param c color of the object
     void drawObject(int row, int col, NamedSymbol symb, NamedColor nc) {
       gg.drawObject(row, col, symb, nc);
     }
