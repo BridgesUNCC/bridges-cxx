3e51cb6dacf2d01ebe381245395aaa7e55cf4a09
Kalpathi Subramanian <krs@uncc.edu>
2021-01-14 20:00:06 -0500

    fixing broken link

--- src/Element.h
+++ src/Element.h
@@ -34,7 +34,13 @@ namespace bridges {
 		 * 	an ElementVisualizer for itself; these are use for styling the
 		 * 	nodes and edges (emanating from the link, if there is a destination
 		 * 	element), respectively.
+
+         * All the tutorials under
+		 * 
+		 * http://bridgesuncc.github.io/tutorials/Overview.html
 		 *
+		 * show how to use different types of elements
+         *
 		 * 	@param E the application data type
 		 *
 		 * 	@author Kalpathi Subramanian
