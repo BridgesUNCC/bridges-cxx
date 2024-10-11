e12625ebd78454f84db894816d532b82bf1eb372
Kalpathi Subramanaian <krs@uncc.edu>
2024-10-11 11:35:00 -0400

    added an abstr class Map.h and changed name to USMap.h

---
+++ src/Map.h
@@ -0,0 +1,17 @@
+#ifndef MAP_H
+#define MAP_H
+
+namespace bridges {
+	namespace datastructure {
+
+		class Map {
+
+			public:
+				virtual const string getProjection() const = 0;
+				virtual const bool getOverlay() const = 0;
+	
+				virtual const string getMapRepresentation() const = 0;
+		};
+	};
+};
+#endif
