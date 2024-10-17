336651fbc51e8666679a80fb98562188575f1da5
Kalpathi Subramanaian <krs@uncc.edu>
2024-10-17 18:14:03 -0400

    updates

---
+++ src/tmp/color.h
@@ -0,0 +1,14 @@
+
+class Color{
+	int r, g, b;	
+	public:
+		Color() {
+			r = 0; g = 0; b = 0;
+		} 
+			
+		int getRed() {return 128;}
+		int getGreen() {return 243;}
+		int getBlue() {return 255;}
+		int getAlpha() {return 255;}
+};
+	
