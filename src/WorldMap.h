0f40526c4868d64d2aadbe239ed98d7f862548f9
Erik Saule <esaule@charlotte.edu>
2024-12-30 15:15:53 -0500

    adding world map support

---
+++ src/WorldMap.h
@@ -0,0 +1,57 @@
+#ifndef WORLD_MAP_H
+
+#define WORLD_MAP_H
+
+#include <math.h>
+#include <cmath>
+
+#include <algorithm>
+
+#include <string>
+#include <vector>
+
+
+#include "DataStructure.h"
+#include "Map.h"
+#include <JSONutil.h>
+
+
+#include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+
+namespace bridges {
+	namespace datastructure {
+
+		using namespace bridges::datastructure;
+
+		class WorldMap :  public Map, public DataStructure {
+			private:
+				virtual const string getDataStructureRepresentation ()
+				const override {
+				  using bridges::JSONUtil::JSONencode;
+					return JSONencode("mapdummy") + COLON + JSONencode(true) + CLOSE_CURLY;
+				}
+			public:
+				const string getProjection() const override {
+					return "equirectangular";
+				}
+				const bool getOverlay() const override {
+					return true;
+				}
+
+				virtual const string getMapRepresentation () const override  {
+					return "\"all\"";
+				}
+			public:
+				WorldMap() {
+				}
+
+				virtual const string getDStype() const override {
+					return "world_map";
+				}
+		};
+	}
+}
+#endif
