7d90bff55e5da11b4ee2499d62cb332352673c12
Kalpathi Subramanian <krs@charlotte.edu>
2025-05-24 19:05:05 -0400

    added 2 letter state codes to each state and embedded in the JSON now

--- src/USMap.h
+++ src/USMap.h
@@ -9,9 +9,11 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using std::string;
 using std::vector;
+using std::unordered_map;
 
 #include "DataStructure.h"
 #include "Map.h"
@@ -59,6 +61,62 @@ namespace bridges {
 					using bridges::JSONUtil::JSONencode;
 					return JSONencode("mapdummy") + COLON + JSONencode(true) + CLOSE_CURLY;
 				}
+				unordered_map<string, string> state_codes = {
+					{"Alabama", "AL"},
+					{"Alaska","AK"},
+					{"Arizona","AZ"},
+					{"Arkansas","AR"},
+					{"California","CA"},
+					{"Colorado","CO"},
+					{"Connecticut","CT"},
+					{"Delaware","DE"},
+					{"District of Columbia","DC"},
+					{"Florida","FL"},
+					{"Georgia","GA"},
+					{"Guam","GU"},
+					{"Hawaii","HI"},
+					{"Idaho","ID"},
+					{"Illinois","IL"},
+					{"Indiana","IN"},
+					{"Iowa","IA"},
+					{"Kansas","KS"},
+					{"Kentucky","KY"},
+					{"Louisiana","LA"},
+					{"Maine","ME"},
+					{"Maryland","MD"},
+					{"Massachusetts","MA"},
+					{"Michigan","MI"},
+					{"Minnesota","MN"},
+					{"Mississippi","MS"},
+					{"Missouri","MO"},
+					{"Montana","MT"},
+					{"Nebraska","NE"},
+					{"Nevada","NV"},
+					{"New Hampshire","NH"},
+					{"New Jersey","NJ"},
+					{"New Mexico","NM"},
+					{"New York","NY"},
+					{"North Carolina","NC"},
+					{"North Dakota","ND"},
+					{"Ohio","OH"},
+					{"Oklahoma","OK"},
+					{"Oregon","OR"},
+					{"Pennsylvania","PA"},
+					{"Puerto Rico","PR"},
+					{"Rhode Island","RI"},
+					{"South Carolina","SC"},
+					{"South Dakota","SD"},
+					{"Tennessee","TN"},
+					{"Texas","TX"},
+					{"Utah","UT"},
+					{"Vermont","VT"},
+					{"Virginia","VA"},
+					{"Virgin Islands","VI"},
+					{"Washington","WA"},
+					{"West Virginia","WV"},
+					{"Wisconsin","WI"},
+					{"Wyoming","WY "}
+				};
 			public:
 				/**
 				 * @brief Gets the type of map projection. For US map we
@@ -90,6 +148,8 @@ namespace bridges {
 						map_str += OPEN_CURLY +
 							QUOTE + "_state_name" + QUOTE + COLON +
 							JSONencode(st.getStateName()) + COMMA +
+							QUOTE + "_state_code" + QUOTE + COLON +
+							JSONencode(state_codes.at(st.getStateName()))+ COMMA +
 							QUOTE + "_stroke_color" + QUOTE + COLON
 							+ st.getStrokeColor().getCSSRepresentation() + COMMA +
 							QUOTE +	"_stroke_width" + QUOTE + COLON +
