98c033e959c86b3bb9d240fbd8c634441bbc82d2
Kalpathi Subramanian <krs@charlotte.edu>
2025-05-26 17:34:21 -0400

    updated data files

--- src/Bridges.h
+++ src/Bridges.h
@@ -604,8 +604,7 @@ namespace bridges {
 				else if (ds_handle->getDStype() == "world_map") {
 					setMap((WorldMap*) ds_handle);
 					ds_json = getJSONHeader()+ds_handle->getDataStructureRepresentation();
-//cout << "JSON(World Map:):\n" << ds_json << "\n"; 
-exit(0);
+//exit(0);
 				}
 				/*
 								else if (ds_handle->getDStype() == "LineChart"){
